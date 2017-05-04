104f81689e3914cef705a84e18f1e0c353e76583
hcy <956885844@qq.com>
2017-05-04 20:57:24 +0800

    hcy for tab

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Tab
 JavaScript实现选项卡。
+选项卡最基本的表现就是，点击bar，显示当前对应的内容，所以有个显示和隐藏的关系在里面。
+<br>
+这个怎么处理呢？“显示当前，隐藏所有的其他” == “隐藏所有，显示当前”
+<br>
+所以我们可以，先将所有的都隐藏了，再把当前这个处理一下。
+<br>
+遍历所有的button，为每一个button加上属性index，即button[i].index，给每一个button绑定上点击事件，<br>
+并且在点击的时候，再次遍历，将所有的button的样式清空，即class置空，并且遍历所有的divDetail，隐藏所有divDetail。<br>
+ 然后，在这层遍历的外面，指定this.className为active（预设好的），并且将this.index对应的divDetail显示出来。<br>
+ 思路就是，所有都隐藏，再显示当前。
---
+++ tab.js
@@ -0,0 +1,26 @@
+/**
+ * Created by hcy on 2017/5/4.
+ */
+window.onload = function () {
+    var button = document.getElementById('topBar').getElementsByTagName('button');
+    console.log(button);
+    var divDetail = document.getElementById('wrap').getElementsByTagName('div');
+    console.log(divDetail)
+
+    // 遍历所有的button，为每一个button加上属性index，即button[i].index，给每一个button绑定上点击事件，
+    // 并且在点击的时候，再次遍历，将所有的button的样式清空，即class置空，并且遍历所有的divDetail，隐藏所有divDetail。
+    // 然后，在这层遍历的外面，指定this.className为active（预设好的），并且将this.index对应的divDetail显示出来。
+    // 思路就是，所有都隐藏，再显示当前。
+    for(var i = 0;i<button.length;i++){
+        button[i].index = i;
+        button[i].onclick = function () {
+            for(var i  = 0;i<button.length;i++){
+                button[i].className = '';
+                divDetail[i].style.display = "none";
+            }
+            this.className = "active";
+            divDetail[this.index].style.display = "block"
+        }
+    }
+
+}
